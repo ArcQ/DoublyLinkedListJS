bfa7a2d620143c5c0f688c8ec1a0a39a7dfe9894
Eddie Law <eddielaw296@gmail.com>
2016-10-15 04:21:22 -0400

    move dev output file to build folder

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,5 +1,6 @@
 var baseConfig = require('./webpack.config.js');
 devConfig = baseConfig;
 devConfig.devtool = 'source-map';
+devConfig.output.filename = "./build/calculator/js";
 module.exports = devConfig;
 
