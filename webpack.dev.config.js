81d1effdce77aba5823050e091dbb8238cd0a5d7
Eddie Law <eddielaw296@gmail.com>
2016-10-15 05:13:59 -0400

    fix npm build scripts

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,6 +1,6 @@
 var baseConfig = require('./webpack.config.js');
 devConfig = baseConfig;
 devConfig.devtool = 'source-map';
-devConfig.output.filename = "./build/calculator/js";
+devConfig.output.filename = "./build/dlinkedlist.js";
 module.exports = devConfig;
 
